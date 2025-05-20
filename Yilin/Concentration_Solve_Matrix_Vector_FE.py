f3d0d6321021d268743d377e1a077b60a2fdd1a0
Yilin-Tang <TangQiaoQiao6@gmail.com>
2025-05-20 21:17:42 +0200

    concentration - v2

--- Yilin/Concentration_Solve_Matrix_Vector_FE.py
+++ Yilin/Concentration_Solve_Matrix_Vector_FE.py
@@ -8,13 +8,14 @@ from matplotlib import rcParams
 import os
 
 phi = 0.15 # porosity
-D = 1.5e-3 # diffusion coefficient
+D = 1.5e-9 # diffusion coefficient
 Peclet = 4.2
-gamma = 0.1
+gamma = 1.67
 T_low = 20
 rp_w = 0.05
 up_inj = 0.05
 
+# reaction parameters
 R = 8.31446261815324 # universal gas constant
 A1 = 3.55e3
 A2 = 1.21
@@ -22,14 +23,12 @@ A3 = -1.33e5
 A4 = -1.41e3
 A5 = 5.08e6
 A6 = -4.55e-4
-
 A = np.ones(4)*4.65e-2 # Arrhenius pre-exponential factor
 Ea = np.ones(4)*34000
 eta0 = 2.0
 theta0 = 0.5
 v = np.ones(4)*1.0
 SA = np.ones(4)*1.0
-
 act = [1.262e-1, 2.575e-3, 4.691e-5, 1.658e-2]
 IAP = np.zeros(4)
 IAP[0] = act[0] * act[1] # NaCl
@@ -37,7 +36,7 @@ IAP[1] = act[0]**2 * act[3] # Na2SO4
 IAP[2] = act[2] * act[1]**2 # SrCl2
 IAP[3] = act[2] * act[3] # SrSO4
 
-C0 = np.array([1.832e-1, 3.912e-3, 1.946e-4, 7.978e-2])
+C0 = np.array([1.832e-1, 3.912e-3, 1.946e-4, 7.978e-2]) # Na, Cl, Sr, SO4
 C_inj = np.array([1.84e-1, 4.0e-3, 1.0e-2, 1.0e-1])
 
 def reaction(C, T):
@@ -47,7 +46,7 @@ def reaction(C, T):
     S = np.zeros(C.shape)
 
     for j in range(num_species):
-        K_diss[j] = A[j] * np.exp(-Ea[j]/(R*T))
+        K_diss[j] = 4.8e-7 #A[j] * np.exp(-Ea[j]/(R*T))
         K_eq[j] = A1 + A2*T + A3/T + A4*np.log10(T) + A5*(T**2) + A6*T**2
         K_eq[j] = np.power(K_eq[j], 10)
 
@@ -77,9 +76,9 @@ def solve_concentration_equation(r0, r1, delta_r, time, delta_t, num_species, T_
     Tc_array = -gamma * delta_t / (r * delta_r)            # additional term for T[i+1]
 
 
-    Tmain_diag = 1 - Ta_array[1:-1] - 2 * Tb_array[1:-1] - Tc_array[1:-1]
-    Tlower_diag = Tb_array[2:-1]                    # length = n_interior - 1
-    Tupper_diag = Ta_array[1:-2] + Tb_array[1:-2] + Tc_array[1:-2]  # length = n_interior - 1
+    Tmain_diag = 1 + Ta_array[1:-1] - 2 * Tb_array[1:-1] + Tc_array[1:-1]
+    Tlower_diag = -Ta_array[2:-1] + Tb_array[2:-1] - Tc_array[2:-1]                    # length = n_interior - 1
+    Tupper_diag = Tb_array[1:-2]  # length = n_interior - 1
 
     TA = diags([Tlower_diag, Tmain_diag, Tupper_diag], offsets=[-1, 0, 1], format='csc')
 
@@ -87,9 +86,9 @@ def solve_concentration_equation(r0, r1, delta_r, time, delta_t, num_species, T_
     Cb_array = delta_t*D / (phi * delta_r**2) * np.ones(Nr + 1)      # appears in the C[i-1] term
     Cc_array = -rp_w*up_inj / phi * delta_t / (r * delta_r)            # additional term for C[i+1]
 
-    Cmain_diag = 1 - Ca_array[1:-1] - 2 * Cb_array[1:-1] - Cc_array[1:-1]
-    Clower_diag = Cb_array[2:-1]                    # length = n_interior - 1
-    Cupper_diag = Ca_array[1:-2] + Cb_array[1:-2] + Cc_array[1:-2]  # length = n_interior - 1
+    Cmain_diag = 1 + Ca_array[1:-1] - 2 * Cb_array[1:-1] + Cc_array[1:-1]
+    Clower_diag = -Ca_array[2:-1] + Cb_array[2:-1] - Cc_array[2:-1]                     # length = n_interior - 1
+    Cupper_diag = Cb_array[1:-2]  # length = n_interior - 1
 
     CA = diags([Clower_diag, Cmain_diag, Cupper_diag], offsets=[-1, 0, 1], format='csc')
 
@@ -101,11 +100,11 @@ def solve_concentration_equation(r0, r1, delta_r, time, delta_t, num_species, T_
         C[:, -1] = C[:, -2]        # enforce outer Neumann BC
         C_interior = C[:, 1:-1].copy()
         T_interior = T[1:-1].copy()
-        C_interior_new = np.array([CA @ C_interior[i, :] for i in range(num_species)]) - delta_t * reaction(C_interior, T_interior) # todo: check A.dot shape
-
+        C_interior_new = np.array([CA @ C_interior[i, :] for i in range(num_species)]) - delta_t * reaction(C_interior, T_interior) 
+        React = reaction(C_interior, T_interior)
         # add boundary contributions:
-        C_interior_new[:, 0] += Cb_array[1] * C[:, 0]
-        C_interior_new[:, -1] += (Ca_array[-2] + Cb_array[-2] + Cc_array[-2]) * C[:, -1]
+        C_interior_new[:, 0] += (-Ca_array[1] + Cb_array[1] - Cc_array[1]) * C[:, 0]
+        C_interior_new[:, -1] += (Cb_array[-2]) * C[:, -1]
 
         C[:,1:-1] = C_interior_new
 
@@ -114,8 +113,8 @@ def solve_concentration_equation(r0, r1, delta_r, time, delta_t, num_species, T_
         T_interior_new = TA.dot(T_interior)
 
         # add boundary contributions:
-        T_interior_new[0] += Tb_array[1] * T[0]
-        T_interior_new[-1] += (Ta_array[-2] + Tb_array[-2] + Tc_array[-2]) * T[-1]
+        T_interior_new[0] += (-Ta_array[1] + Tb_array[1] - Tc_array[1]) * T[0]
+        T_interior_new[-1] += (Tb_array[-2]) * T[-1]
 
         T[1:-1] = T_interior_new
 
@@ -131,9 +130,9 @@ Tp_low = 20
 
 rp_out = 5
 delta_r = 0.1
-delta_t = 1e-4
+delta_t = 1e-2
 
-ITER = 1e5
+ITER = 10000
 final_time_p = ITER * delta_t
 print(f"Final time: {final_time_p:.2f} seconds")
 r, t, T, C, T_history, C_history = solve_concentration_equation(rp_w, rp_out, delta_r, final_time_p, delta_t, 4, Tp_high, Tp_low, C0, C_inj)
