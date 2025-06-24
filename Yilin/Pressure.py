b2025b0ba29e3c8edaba0d562bfcef017cc52947
Yilin-Tang <TangQiaoQiao6@gmail.com>
2025-06-24 23:11:48 +0200

    implement pressure

--- Yilin/Pressure.py
+++ Yilin/Pressure.py
@@ -11,12 +11,13 @@ def solve_pressure_equation(r0, r1, N, p0, dp0):
     p[1] = p0 + h * dp0  # Forward Euler to estimate p[1]
 
     for i in range(1, N):
-        p[i + 1] = -(-2 + h/(r0 + i*h)) * p[i] - (1 - h/(r0 + i*h)) * p[i - 1]
+        ri = r0 + i*h
+        p[i + 1] = (2*ri + h)/(ri + h) * p[i] - ri/(ri + h) * p[i - 1]
 
     return r, p
 
-r_w = 1 # radius of well
-r_out = r_w + 5
+r_w = 0.05 # radius of well
+r_out = 5
 
 kappa = 1e3 # k/mu
 u_inj = 10 # Darcy velocity of injection
@@ -24,16 +25,16 @@ dp_w = -1/kappa * u_inj
 p_w = 10 # pressure at rw, which can be assigned arbitrarily
 
 # Solve and plot
-r, p = solve_pressure_equation(r_w, r_out, N=100, p0=p_w, dp0=dp_w)
+r, p = solve_pressure_equation(r_w, r_out, N=10000, p0=p_w, dp0=dp_w)
 
 c2 = 1/kappa * r_w * u_inj * np.log(r_w) + p_w
 p_ex = - 1/kappa * r_w * u_inj * np.log(r) + c2
 
 plt.plot(r, p, label='Numerical solution')
-plt.plot(r, p_ex, label='Exact solution')
+plt.plot(r, p_ex, label='Exact solution', linestyle='--')
 plt.xlabel('r')
 plt.ylabel('p(r)')
-plt.title('Solution of r d²p/dr² + dp/dr = 0')
+plt.title('Solution of Pressure')
 plt.grid()
 plt.legend()
 plt.show()
